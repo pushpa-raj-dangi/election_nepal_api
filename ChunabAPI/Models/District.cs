ed3a4c36bcfec207eab2c2a74117e661fb56f23b
pushpa-raj-dangi <dcpushparaj@gmail.com>
2021-07-12 16:19:33 +0545

    district class added

---
+++ ChunabAPI/Models/District.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChunabAPI.Models
+{
+    public class District
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public string Voters { get; set; }
+
+
+    }
+}
