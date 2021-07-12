c3f157c0ef93f4c76645f55e752e4a9581c7a6d1
pushpa-raj-dangi <dcpushparaj@gmail.com>
2021-07-12 16:28:58 +0545

    implement district controler

---
+++ ChunabAPI/Controllers/DistrictsController.cs
@@ -0,0 +1,55 @@
+﻿using ChunabAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChunabAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<District>> GetDistricts()
+        {
+            var id = Guid.NewGuid();
+            var districts = new List<District>() { new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" } };
+            return Ok(districts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<District> GetDistrict(Guid id)
+        {
+            var districts = new List<District>() { new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" } };
+            var district = districts.Find(x => x.Id == id);
+            return Ok(district);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<District> DeleteDistrict(Guid id)
+        {
+            var districts = new List<District>() { new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" } };
+            var district = districts.Find(x => x.Id == id);
+            var delteDistrict = districts.Remove(district);
+            return Ok(delteDistrict);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<District> DeleteDistrict(Guid id, District districtFromBody)
+        {
+            var districts = new List<District>() { new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" }, new District { Id = id, Name = "Rolpa", Description = "Rolpa is ", Photo = "photos0", Voters = "234242342" } };
+            var district = districts.Find(x => x.Id == id);
+
+                district.Name  = districtFromBody.Name,
+                district.Photo = districtFromBody.Photo;
+                district.Voters = districtFromBody.Voters;
+                district.Description = districtFromBody.Description;
+
+            return Ok(districtFromBody);
+        }
+    }
+
+}
